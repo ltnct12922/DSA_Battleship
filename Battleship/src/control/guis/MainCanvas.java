d6a9972ca0dace6315aac82b3d2d4da7a43b15da
Le Tinh Nhut <87026371+ltnct12922@users.noreply.github.com>
2023-04-18 16:29:30 +0700

    Update MainCanvas.java

--- Battleship/src/control/guis/MainCanvas.java
+++ Battleship/src/control/guis/MainCanvas.java
@@ -4,20 +4,10 @@ import control.components.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class MainCanvas extends JPanel implements Runnable, KeyListener {
+public class MainCanvas extends JPanel implements Runnable {
 
-    public static int animationClock = 0;
     public static AudioPlayer audioPlayer;
-
     private long sleepTime = 20;
     Thread thread;
     private boolean endGame;
@@ -36,52 +26,16 @@ public class MainCanvas extends JPanel implements Runnable, KeyListener {
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (Exception e) {
-        }
-        while (!isEndGame()) {
-            animationClock++;
-            if (animationClock == 2147483647)
-                animationClock = 0;
-            // Repaint
-            repaint();
             try {
                 Thread.sleep(sleepTime);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-    }
 
     public void paintComponent(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, GUI.WIDTH_FRAME, GUI.HEIGHT_FRAME);
-    }
-
-    private void drawRightMenu(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.fillRect(800, 0, 200, 544);
-        g.translate(800, 0);
-        g.setColor(Color.WHITE);
-        g.translate(0, 0);
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    public boolean isEndGame() {
-        return endGame;
     }
 
     public void setEndGame(boolean endGame) {
