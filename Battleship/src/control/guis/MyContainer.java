b554f2a0ea04bac8c38d8aace077ab9e254f7dd7
huythanh1324 <huythanh1324@gmail.com>
2023-04-20 21:46:43 +0700

    Link game to menu penal

--- Battleship/src/control/guis/MyContainer.java
+++ Battleship/src/control/guis/MyContainer.java
@@ -1,16 +1,22 @@
 package control.guis;
 
 import java.awt.CardLayout;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
-import javax.swing.JPanel;
 
-public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu{
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu, KeyListener{
     public static final String PLAY = "PLAY";
     public static final String MENU = "MENU";
     private MainCanvas mainCanvas;
     private PanelMenu panelMenu;
     private CardLayout cardLayout;
+    private GamePanel gamePanel;
     public MyContainer() throws IOException {
         initPanel();
         initComponents();
@@ -37,12 +43,30 @@ public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu{
 
     @Override
     public void showGame() {
-        mainCanvas = new MainCanvas();
-        mainCanvas.setOnBackToMenuListener(this);
-        add(mainCanvas, PLAY);
-        cardLayout.show(this, PLAY);
-        mainCanvas.requestFocus();
-        mainCanvas.start();
+        // mainCanvas = new MainCanvas();
+        // mainCanvas.setOnBackToMenuListener(this);
+        // add(mainCanvas, PLAY);
+        // cardLayout.show(this, PLAY);
+        // mainCanvas.requestFocus();
+        // mainCanvas.start();
+        String[] options = new String[] {"Easy", "Medium", "Hard"};
+        String message = "Easy will make moves entirely randomly,\nMedium will focus on areas where it finds ships,"
+                + "\nand Hard will make smarter choices over Medium.";
+        int difficultyChoice = JOptionPane.showOptionDialog(null, message,
+                "Choose an AI Difficulty",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+        JFrame frame = new JFrame("Battleship");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        gamePanel = new GamePanel(difficultyChoice);
+        frame.getContentPane().add(gamePanel);
+
+        frame.addKeyListener(this);
+        frame.pack();
+        frame.setVisible(true);
     }
 
     @Override
@@ -50,4 +74,16 @@ public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu{
         cardLayout.show(this, MENU);
         this.remove(mainCanvas);
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        gamePanel.handleInput(e.getKeyCode());
+    }
+
+  
+    @Override
+    public void keyTyped(KeyEvent e) {}
+ 
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
