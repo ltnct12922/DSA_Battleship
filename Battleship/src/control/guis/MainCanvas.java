33d8efcabc5bee8f18f32a1047da934552922678
Le Tinh Nhut <87026371+ltnct12922@users.noreply.github.com>
2023-05-31 12:10:18 +0700

    Update MainCanvas.java

--- Battleship/src/control/guis/MainCanvas.java
+++ Battleship/src/control/guis/MainCanvas.java
@@ -1,43 +1,58 @@
 package control.guis;
 
-import control.components.*;
-
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MainCanvas extends JPanel implements Runnable {
+public class MainCanvas extends JPanel implements KeyListener {
 
-    public static AudioPlayer audioPlayer;
-    private long sleepTime = 20;
-    Thread thread;
-    private boolean endGame;
-    public IBackToMenu onBackToMenuListener;
+    Thread thread; 
+    private GamePanel gamePanel;
+    private int difficultyChoice;
 
-    public MainCanvas() {
-        this.setEndGame(false);
+    public MainCanvas() {String[] options = new String[] {"Easy", "Medium", "Hard"};
+    String message = "Easy will make moves entirely randomly,\nMedium will focus on areas where it finds ships,"
+            + "\nand Hard will make smarter choices over Medium.";
+    difficultyChoice = JOptionPane.showOptionDialog(null, message,
+            "Choose an AI Difficulty",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+            null, options, options[0]);
+
+    // JFrame frame = new JFrame("Battleship");
+    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // frame.setResizable(false);
+ 
+    
+    // frame.getContentPane().add(gamePanel);
+
+        this.addKeyListener(this);
+    // frame.pack();
+    // frame.setVisible(true);
+        //this.setEndGame(false);
         this.setSize(GUI.WIDTH_FRAME, GUI.HEIGHT_FRAME);
     }
-
+ 
     public void start() {
+        gamePanel = new GamePanel(difficultyChoice);
+        add(gamePanel);
         thread.start();
-        audioPlayer.playSound(AudioPlayer.GAME_START);
     }
-
+    // public void paintComponent(Graphics g) {
+    //     g.setColor(Color.BLACK);
+    //     g.fillRect(0, 0, GUI.WIDTH_FRAME, GUI.HEIGHT_FRAME);
+    // }
 
     @Override
-    public void run() {
+    public void keyTyped(KeyEvent e) {
+
     }
 
-    public void paintComponent(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, GUI.WIDTH_FRAME, GUI.HEIGHT_FRAME);
+    @Override
+    public void keyPressed(KeyEvent e) {
+        gamePanel.handleInput(e.getKeyCode());
     }
 
-    public void setEndGame(boolean endGame) {
-        this.endGame = endGame;
-    }
-
-    public void setOnBackToMenuListener(IBackToMenu onBackToMenuListener) {
-        this.onBackToMenuListener = onBackToMenuListener;
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 }
