cd9cc036195465df7e3347e3452037b268432733
Le Tinh Nhut <87026371+ltnct12922@users.noreply.github.com>
2023-05-31 12:10:58 +0700

    Update MyContainer.java

--- Battleship/src/control/guis/MyContainer.java
+++ Battleship/src/control/guis/MyContainer.java
@@ -1,16 +1,18 @@
 package control.guis;
 
 import java.awt.CardLayout;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
-
 import javax.swing.JPanel;
 
-public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu{
+public class MyContainer extends JPanel implements IActionShowGame, KeyListener{
     public static final String PLAY = "PLAY";
     public static final String MENU = "MENU";
     private MainCanvas mainCanvas;
     private PanelMenu panelMenu;
     private CardLayout cardLayout;
+    private GamePanel gamePanel;
     public MyContainer() throws IOException {
         initPanel();
         initComponents();
@@ -25,10 +27,9 @@ public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu{
 
     private void initComponents() throws IOException {
         panelMenu = new PanelMenu();
-
         panelMenu.setiActionShowGame(this);
     }
-
+ 
     private void addComponents() {
 
         add(panelMenu, MENU);
@@ -38,16 +39,22 @@ public class MyContainer extends JPanel implements IActionShowGame, IBackToMenu{
     @Override
     public void showGame() {
         mainCanvas = new MainCanvas();
-        mainCanvas.setOnBackToMenuListener(this);
         add(mainCanvas, PLAY);
         cardLayout.show(this, PLAY);
         mainCanvas.requestFocus();
         mainCanvas.start();
+        
     }
 
     @Override
-    public void backToMenu() {
-        cardLayout.show(this, MENU);
-        this.remove(mainCanvas);
+    public void keyPressed(KeyEvent e) {
+        gamePanel.handleInput(e.getKeyCode());
     }
+
+  
+    @Override
+    public void keyTyped(KeyEvent e) {}
+ 
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
